bef80c81cef688792b612efe812fdb42da4ab2ef
mpsmcgrath <mpsmcgrath@gmail.com>
2017-04-19 21:52:59 +0100

    Updated SQL file!

--- mysql_import.sql
+++ mysql_import.sql
@@ -1,5 +1,30 @@
-USE blog_sample; 
-CREATE TABLE IF NOT EXISTS `products` (
+CREATE DATABASE  IF NOT EXISTS `blog_sample` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `blog_sample`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: blog_sample
+-- ------------------------------------------------------
+-- Server version	5.7.17-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `product_code` varchar(60) NOT NULL,
   `product_name` varchar(60) NOT NULL,
@@ -8,23 +33,81 @@ CREATE TABLE IF NOT EXISTS `products` (
   `price` decimal(10,2) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `product_code` (`product_code`)
-) AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `product_code`, `product_name`, `product_desc`, `product_img_name`, `price`) VALUES
-(1, 'FOOD01', 'Coca-Cola Cans', '12 Pack x 330ml, guaranteed to rot your teeth', 'coca-cola.jpg', 8.40),
-(2, 'FOOD02', 'Sprouts', 'A beautiful sprout, grown in Brussels', 'sprout.jpg', 25.00),
-(3, 'FOOD03', 'Cadbury Easter Egg', 'Cadbury Dairy Milk Oreo Large Easter Egg 278G', 'easter-egg.jpg', 9.99),
-(4, 'FOOD04', 'Kelloggs Pop Tarts', 'Strawberry Sensation 400G. Packed full of nutritious sugar.', 'pop-tarts.jpg', 3.70),
-(5, 'SPRT05', 'ADIDAS Football', 'Adidas Finale Cardiff 2017 Capitano Ball, White', 'football.jpg', 25.00),
-(6, 'SPRT06', 'Golf Ball', 'Professional quality golf ball, White', 'golfball.jpg', 5.00),
-(7, 'SPRT07', 'Basket Ball', 'A basketball, as thrown and bounced by Basketballers', 'basketball.jpg', 15.00),
-(8, 'SPRT08', 'Rugby Ball', 'Regulation sized rugby ball, for playing rugby with', 'rugbyball.jpg', 19.99),
-(9, 'DRNK09', 'Buckfast Wine', 'Anti-social behaviour, bottled by monks apparently', 'buckfast.jpg', 12.00),
-(10, 'DRNK10', 'Vodka', 'Russian quality vodka, made from potatos and tears', 'vodka.jpg', 25.00),
-(11, 'DRNK11', 'Absinthe', 'Quite strong drink, do not operate heavy machinary', 'absinthe.jpg', 37.00),
-(12, 'DRNK12', 'Poitin', 'Traditional Irish drink with small chance of death', 'poitin.jpg', 82.50);
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` VALUES (1,'FOOD01','Coca-Cola Cans','12 Pack x 330ml, guaranteed to rot your teeth','coca-cola.jpg',8.40),(2,'FOOD02','Sprouts','A beautiful sprout, grown in Brussels','sprout.jpg',25.00),(3,'FOOD03','Cadbury Easter Egg','Cadbury Dairy Milk Oreo Large Easter Egg 278G','easter-egg.jpg',9.99),(4,'FOOD04','Kelloggs Pop Tarts','Strawberry Sensation 400G. Packed full of nutritious sugar.','pop-tarts.jpg',3.70),(5,'SPRT05','ADIDAS Football','Adidas Finale Cardiff 2017 Capitano Ball, White','football.jpg',25.00),(6,'SPRT06','Golf Ball','Professional quality golf ball, White','golfball.jpg',5.00),(7,'SPRT07','Basket Ball','A basketball, as thrown and bounced by Basketballers','basketball.jpg',15.00),(8,'SPRT08','Rugby Ball','Regulation sized rugby ball, for playing rugby with','rugbyball.jpg',19.99),(9,'DRNK09','Buckfast Wine','Anti-social behaviour, bottled by monks apparently','buckfast.jpg',12.00),(10,'DRNK10','Vodka','Russian quality vodka, made from potatos and tears','vodka.jpg',25.00),(11,'DRNK11','Absinthe','Quite strong drink, do not operate heavy machinary','absinthe.jpg',37.00),(12,'DRNK12','Poitin','Traditional Irish drink with small chance of death','poitin.jpg',82.50);
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `tblproduct`
+--
+
+DROP TABLE IF EXISTS `tblproduct`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tblproduct` (
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `image` text NOT NULL,
+  `price` double(10,2) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `product_code` (`code`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tblproduct`
+--
+
+LOCK TABLES `tblproduct` WRITE;
+/*!40000 ALTER TABLE `tblproduct` DISABLE KEYS */;
+INSERT INTO `tblproduct` VALUES (1,'3D Camera','3DcAM01','product-images/camera.jpg',1500.00),(2,'External Hard Drive','USB02','product-images/external-hard-drive.jpg',800.00),(3,'Wrist Watch','wristWear03','product-images/watch.jpg',300.00);
+/*!40000 ALTER TABLE `tblproduct` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `userId` int(11) NOT NULL AUTO_INCREMENT,
+  `userName` varchar(30) NOT NULL,
+  `userEmail` varchar(60) NOT NULL,
+  `userPass` varchar(255) NOT NULL,
+  PRIMARY KEY (`userId`),
+  UNIQUE KEY `userEmail` (`userEmail`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Sean','mdfsdq@fgfgh.com','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92'),(2,'john','john@johnmail.com','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92'),(3,'Sean','sean@sean.com','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92'),(4,'Sean','sean@sean.sean','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-04-19 21:52:27
